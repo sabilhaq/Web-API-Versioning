ba2eeff9c0c1b070d06e20cb73c1ed53031da2fe
Makrifat Sabilhaq <makrifat.sabilhaq@edot.id>
2024-12-19 14:14:39 +0700

    initial commit

---
+++ Web-API-Versioning/Web-API-Versioning.API/ConfigureSwaggerOptions.cs
@@ -0,0 +1,40 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Web_API_Versioning.API
+{
+    public class ConfigureSwaggerOptions : IConfigureNamedOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider apiVersionDescriptionProvider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider apiVersionDescriptionProvider)
+        {
+            this.apiVersionDescriptionProvider = apiVersionDescriptionProvider;
+        }
+        public void Configure(string? name, SwaggerGenOptions options)
+        {
+            Configure(options);
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var item in apiVersionDescriptionProvider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(item.GroupName, CreateVersionInfo(item));
+            }
+        }
+
+        private OpenApiInfo CreateVersionInfo(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo
+            {
+                Title = "Your Versioned API",
+                Version = description.ApiVersion.ToString()
+            };
+
+            return info;
+        }
+    }
+}
---
+++ Web-API-Versioning/Web-API-Versioning.API/Program.cs
@@ -0,0 +1,53 @@
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using Web_API_Versioning.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0); //same as ApiVersion.Default
+    options.ReportApiVersions = true;
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
+builder.Services.AddHttpContextAccessor();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+
+var app = builder.Build();
+
+var versionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var description in versionDescriptionProvider.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+        }
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
